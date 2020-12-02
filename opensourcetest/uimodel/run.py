8a9ebda70951d1d2dfcc7f21fd9807429c28c020
luozhongwen <luozw@inhand.com.cn>
2020-12-02 18:43:53 +0800

    add run.py

--- opensourcetest/uimodel/run.py
+++ opensourcetest/uimodel/run.py
@@ -5,7 +5,7 @@ from loguru import logger
 from opensourcetest.builtin.baseUiRunner import ost_ui_cmd_runner, ost_ui_runner
 
 if __name__ == "__main__":
-    browser = ost_ui_runner("chrome", "close", "remote")
+    browser = ost_ui_runner("chrome", "close", "local")
     url = 'Local Test Report Address:http://127.0.0.1:63342/'+os.getcwd().split("\\")[-1]+f'/Report/{browser.replace(" ", "_")}/allure-report/index.html '
     logger.info(url)
 
