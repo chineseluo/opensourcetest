13fc62915271442d089da58ee22cd39984bbc17d
luozhongwen <luozw@inhand.com.cn>
2020-12-23 14:54:32 +0800

    add app model

--- opensourcetest/appmodel/__init__.py
+++ opensourcetest/appmodel/__init__.py
@@ -1,13 +1,2 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
-
-"""
-------------------------------------
-@Project : opensourcetest
-@Time    : 2020/12/23 14:23
-@Auth    : luozhongwen
-@Email   : luozw@inhand.com.cn
-@File    : __init__.py
-@IDE     : PyCharm
-------------------------------------
-"""
