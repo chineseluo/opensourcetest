350d108cfeec4c8f208a04d838208997f81d02e1
luozhongwen <luozw@inhand.com.cn>
2021-01-11 14:47:24 +0800

    add __main__.py

---
+++ opensourcetest/__main__.py
@@ -0,0 +1,17 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+------------------------------------
+@Project : opensourcetest
+@Time    : 2021/1/11 14:45
+@Auth    : chineseluo
+@Email   : 848257135@qq.com
+@File    : __main__.py
+@IDE     : PyCharm
+------------------------------------
+"""
+from opensourcetest.cli import main
+
+if __name__ == '__main__':
+    main()
